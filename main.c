7d0e3e0cc5ff5b33a512c6e6772b090fc106fe90
Ruiber <pedrosoumartins014@gmail.com>
2020-06-24 15:40:07 -0300

    Created Huffman tree.

--- main.c
+++ main.c
@@ -7,18 +7,22 @@ typedef struct huff_tree{
 }huff;
 
 int *freq_table(char *fileName);
-void insert(huff *heap, huff node);
-void up(huff *heap, int pos);
-huff *pop(huff *heap, huff node);
-void down(huff *heap, int pos);
-void populate(huff *heap, int *freq);
+void insert(huff *heap[], huff *node);
+void up(huff *heap[], int pos);
+huff *pop(huff *heap[]);
+void down(huff *heap[], int pos);
+void populate(huff *heap[], int *freq);
+huff *huffman(huff *heap[]);
 
 int main(){
+    huff *root;
     int *freq = freq_table("input.txt");
     int i;
-    huff heap[257];
+    huff *heap[257];
     
     populate(heap, freq);
+
+    root = huffman(heap);
 
     free(freq);
     
@@ -39,14 +43,14 @@ int *freq_table(char *fileName){
     return freq;
 }
 
-void insert(huff *heap, huff node){
-    heap[++heap[0].freq] = node;
-    up(heap, heap[0].freq);
+void insert(huff *heap[], huff *node){
+    heap[++heap[0]->freq] = node;
+    up(heap, heap[0]->freq);
 }
 
-void up(huff *heap, int pos){
-    huff aux;
-    if((heap[pos].freq < heap[pos/2].freq) && pos > 1){
+void up(huff *heap[], int pos){
+    huff *aux;
+    if((heap[pos]->freq < heap[pos/2]->freq) && pos > 1){
         aux = heap[pos];
         heap[pos] = heap[pos/2];
         heap[pos/2] = aux;
@@ -54,28 +58,28 @@ void up(huff *heap, int pos){
     }
 }
 
-huff *pop(huff *heap, huff node){
-    huff *aux = &heap[1];
-    heap[1] = heap[heap[0].freq--];
+huff *pop(huff *heap[]){
+    huff *aux = heap[1];
+    heap[1] = heap[heap[0]->freq--];
     down(heap, 1);
     return aux;
 }
 
-void down(huff *heap, int pos){
-    int qnt = heap[0].freq;
+void down(huff *heap[], int pos){
+    int qnt = heap[0]->freq;
     int least;
-    huff aux;
+    huff *aux;
     if(qnt >= 2*pos + 1){
-        if(heap[2*pos].freq > heap[2*pos + 1].freq) least = 2*pos + 1;
+        if(heap[2*pos]->freq > heap[2*pos + 1]->freq) least = 2*pos + 1;
         else least = 2*pos;
-        if(heap[least].freq < heap[pos].freq){
+        if(heap[least]->freq < heap[pos]->freq){
             aux = heap[pos];
             heap[pos] = heap[least];
             heap[least] = aux;
             down(heap, least);
         }
     }
-    else if(qnt == 2*pos && heap[pos].freq > heap[2*pos].freq){
+    else if(qnt == 2*pos && heap[pos]->freq > heap[2*pos]->freq){
         aux = heap[pos];
         heap[pos] = heap[2*pos];
         heap[2*pos] = aux;
@@ -83,19 +87,36 @@ void down(huff *heap, int pos){
     }
 }
 
-void populate(huff *heap, int *freq){
+void populate(huff *heap[], int *freq){
     int i;
-    huff aux;
-    heap[0].freq = 0;
-    heap[0].ch = -2;
-    heap[0].left = NULL;
-    heap[0].right = NULL;
+    huff *aux;
+    heap[0] = (huff *)malloc(sizeof(huff));
+    heap[0]->freq = 0;
+    heap[0]->ch = -2;
+    heap[0]->left = NULL;
+    heap[0]->right = NULL;
     for(i = 0; i < 256; i++){
         if(freq[i] == 0) continue;
-        aux.freq = freq[i];
-        aux.ch = i;
-        aux.left = NULL;
-        aux.right = NULL;
+        aux = (huff *)malloc(sizeof(huff));
+        aux->freq = freq[i];
+        aux->ch = i;
+        aux->left = NULL;
+        aux->right = NULL;
         insert(heap, aux);
     }
+}
+
+huff *huffman(huff *heap[]){
+    huff *root, *aux1, *aux2;
+    while(heap[0]->freq > 1){
+        aux1 = pop(heap);
+        aux2 = pop(heap);
+        root = (huff *)malloc(sizeof(huff));
+        root->ch = -1;
+        root->freq = aux1->freq + aux2->freq;
+        root->right = aux1;
+        root->left = aux2;
+        insert(heap, root);
+    }
+    return root;
 }
